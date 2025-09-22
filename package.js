a3f89649721f3b668c18728d01b63028ee7f41ff
kakashiNN <do0fluxo@gmail.com>
2025-09-22 18:43:23 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "pindown-api",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.7.0",
+    "express": "^4.19.0",
+    "form-data": "^4.0.0"
+  }
+      }
