a7339bc1f7943139f822965cbeb875ddc1331a5d
kakashiNN <do0fluxo@gmail.com>
2025-09-22 18:46:19 +0600

    Update package.js

--- package.js
+++ package.js
@@ -6,9 +6,12 @@
   "scripts": {
     "start": "node index.js"
   },
+  "engines": {
+    "node": "18.x"
+  },
   "dependencies": {
     "axios": "^1.7.0",
     "express": "^4.19.0",
     "form-data": "^4.0.0"
   }
-      }
+}
